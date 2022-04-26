08aa5dccea75d0bdf5b33686244df4ec816bde28
Noah Reece <noah.w.reece@gmail.com>
2022-04-26 15:35:38 -0400

    commit tree be1aa325d73df2131ed307449117c962c611c8c5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  # validate :title_not_true_facts
+  # validate :click_bates
+
+  # Custom Validations
+  # def title_not_true_facts
+  #   if title == 'True Facts'
+  #     errors.add(:title, "Cannot have title of \"True Facts\"")
+  #   end
+  # end
+
+  # def click_bates
+  #   unless title =~ /Won't Believe|Secret|Top [0-9]+|Guess/
+  #     errors.add(:title, 'Use more buzzwords in your title, bro')
+  #   end
+  # end
 end
